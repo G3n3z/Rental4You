121128925525b6153c748ea15f71ff1bc70d7f78
jorhug <hugomsjorge@gmail.com>
2022-12-23 19:16:33 +0000

    data annotations nos modelos e controlador dos users verificações e proteções nos users

--- ViewModel/UserDetailsViewModel.cs
+++ ViewModel/UserDetailsViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using Rental4You.Models;
 
 namespace Rental4You.ViewModel
@@ -6,9 +9,21 @@ namespace Rental4You.ViewModel
     public class UserDetailsViewModel
     {
         public string Id;
+
+        [DisplayName("Primeiro Nome")]
         public string PrimeiroNome { get; set; }
+
+        [DisplayName("Último Nome")]
         public string UltimoNome { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "Necessita indicar a data de nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
+
+        [StringLength(9, ErrorMessage = "O {0} tem de ser composto por {1} digitos.", MinimumLength = 9)]
+        [RegularExpression(@"[0-9]{9}$", ErrorMessage = "O NIF tem de ser composto por 9 digitos.")]
+        [Required(ErrorMessage = "Necessita indicar número de contribuinte")]
         public string NIF { get; set; }
         public int? EmpresaId { get; set; }
         public bool Active { get; set; }
